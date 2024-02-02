38b7b89289a24e6b0ce44809502aae69c4f0f675
Volodymyr Riabenko <vova.riabenko@gmail.com>
2024-02-02 22:11:02 +0200

    Add models

---
+++ WeatherForecast.UI/src/models/models.ts
@@ -0,0 +1,31 @@
+﻿export interface WeatherDataModel {
+  latitude: number;
+  longitude: number;
+  time: string;
+  weatherCode: number;
+  maxTemperature: number;
+  minTemperature: number;
+  precipitationSum: number;
+  rainSum: number;
+  maxWindSpeed: number;
+}
+
+export interface WindInfoModel {
+  speed: number;
+}
+
+export interface MainWeatherInfoModel {
+  temperature: number;
+  humidity: number;
+}
+
+export interface WeatherInfoModel {
+  type: string;
+  description: string;
+}
+
+export interface WeatherReportModel {
+  weather: WeatherInfoModel[];
+  main: MainWeatherInfoModel;
+  wind: WindInfoModel;
+}
